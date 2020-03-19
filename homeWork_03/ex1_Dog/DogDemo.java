8cb38c1d40f3fc58014e00da015f0613576ca08b
Roman <ilovebfface@gmail.com>
2020-03-19 14:26:04 +0300

    DogDemo.java is added

---
+++ homeWork_03/ex1_Dog/DogDemo.java
@@ -0,0 +1,41 @@
+public class DogDemo {
+
+  public static void main(String[] args) {
+    Dog tuzik = new Dog("Tuzik",2,"Brown");
+
+    tuzik.voice();
+    tuzik.eat();
+    tuzik.sleep();
+
+  }
+
+  public static class Dog {
+    public int age;
+    public String color;
+    public String name;
+
+    public Dog(String name, int age, String color) {
+      this.name = name;
+      this.age = age;
+      this.color = color;
+      System.out.println("Dog '" + name + "' say hi to you!");
+      System.out.println("You see " + color + " color dog " + age + " years old");
+    }
+
+    public void voice() {
+      print("shouting");
+    }
+
+    public void eat() {
+      print("eating");
+    }
+
+    public void sleep() {
+      print("sleeping");
+    }
+
+    public void print(String action) {
+      System.out.println(name + " is " + action);
+    }
+  }
+}
