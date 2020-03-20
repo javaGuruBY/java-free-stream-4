f293cbbb0ff26994939eb15e803dcca59f91c88f
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2020-03-20 14:08:47 +0300

    Fix RandomGeneratorDemo.java(delete comments)
    fixed file

--- homeWork_02/ex4_RandomNumberGenerator/Artemiy_Stankevich/RandomGeneratorDemo.java
+++ homeWork_02/ex4_RandomNumberGenerator/Artemiy_Stankevich/RandomGeneratorDemo.java
@@ -1,13 +1,13 @@
-import java.util.Random; //Импортируем пакет Random
+import java.util.Random; 
 
 public class RandomGeneratorDemo {
     public static void main(String[] args) {
-        Random randomGen = new Random(); //Создаем экземпляр класса (обект) типа Random для исп. его методов.
+        Random randomGen = new Random(); 
         int num = 100;
 
-        int firstNum = randomGen.nextInt(num); //Используем метод nextInt() через точечную нотацию
-        int secondNum = randomGen.nextInt(num);// то же самое
-        int thirdNum = randomGen.nextInt(num); // то же самое
+        int firstNum = randomGen.nextInt(num); 
+        int secondNum = randomGen.nextInt(num);
+        int thirdNum = randomGen.nextInt(num); 
 
         System.out.println("Первое сгенерированное число = " + firstNum);
         System.out.println("Второе сгенерированное число = " + secondNum);
