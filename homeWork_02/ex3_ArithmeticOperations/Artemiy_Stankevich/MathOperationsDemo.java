5015a513fc1c08122ddf25b0ba507988ec071924
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2020-03-20 14:06:30 +0300

    Fix my homework (delete comments)
    fixed homework

--- homeWork_02/ex3_ArithmeticOperations/Artemiy_Stankevich/MathOperationsDemo.java
+++ homeWork_02/ex3_ArithmeticOperations/Artemiy_Stankevich/MathOperationsDemo.java
@@ -1,13 +1,13 @@
 public class MathOperationsDemo {
     public static void main(String[] args) {
-        int firstNum = 10; // Первое целое число
-        int secondNum = 25; // Второе целое число
-// ============== Переменные хранящие в себе математические операции ===============================
+        int firstNum = 10;
+        int secondNum = 25; 
+
         int add = firstNum + secondNum;
         int sub = firstNum - secondNum;
         int mult = firstNum * secondNum;
-        double div = (double)firstNum / secondNum; //явно приводим одно из чисел к типу double, чтобы не отпала дробная часть.
-// ============== ВЫВОД В КОНСОЛЬ РЕЗУЛЬТАТОВ ====================================        
+        double div = (double)firstNum / secondNum; 
+      
         System.out.println("First operation 'addition' firstNum + secondNum = " + add);
         System.out.println("Second operation 'subtraction' firstNum - secondNum = " + sub);
         System.out.println("Third operation 'multiplication' firstNum * secondNum = " + mult);
