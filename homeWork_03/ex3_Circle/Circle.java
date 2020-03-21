b0c79dc31f2553e549f5f41f3ca4cff7e9e96aa2
Roman <ilovebfface@gmail.com>
2020-03-21 23:10:26 +0300

    Roman Hw3 Task9

---
+++ homeWork_03/ex3_Circle/Circle.java
@@ -0,0 +1,16 @@
+package login;
+
+public class Circle {
+
+  public double radius;
+
+  public Circle(double radius) {
+    this.radius = radius;
+  }
+
+  public double calculateArea() {
+    double area = Math.PI*radius*radius;
+    System.out.println("Circle area = " + area);
+    return area;
+  }
+}
