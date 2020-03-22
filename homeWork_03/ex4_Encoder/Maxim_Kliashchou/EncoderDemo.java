db66bae86950f88469755518e5bd571d498bc13d
Maxim Kliashchou <61432134+maximkliashchou@users.noreply.github.com>
2020-03-22 22:16:24 +0300

    add encoder demo class

---
+++ homeWork_03/ex4_Encoder/Maxim_Kliashchou/EncoderDemo.java
@@ -0,0 +1,8 @@
+public class EncoderDemo {
+    public static void main(String[] args) {
+        Encoder encoder = new Encoder();
+
+        encoder.encode((short)65);
+        encoder.decode('X');
+    }
+}
