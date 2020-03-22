464df31104fc0b9a4788e4884ede6941b571f63e
Maxim Kliashchou <61432134+maximkliashchou@users.noreply.github.com>
2020-03-22 21:37:18 +0300

    add human demo class

---
+++ homeWork_03/ex2_Human/Maxim_Kliashchou/HumanDemo.java
@@ -0,0 +1,9 @@
+public class HumanDemo {
+    public static void main(String[] args) {
+        Human humanOne = new Human("Max", 19);
+        Human humanTwo = new Human("Eve", 20);
+
+        humanOne.greet();
+        humanTwo.greet();
+    }
+}
