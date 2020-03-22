ce330536f8cfbdd11347ed96488e0335a93abcc2
Maxim Kliashchou <61432134+maximkliashchou@users.noreply.github.com>
2020-03-22 22:15:14 +0300

    add encode class

---
+++ homeWork_03/ex4_Encoder/Maxim_Kliashchou/Encoder.java
@@ -0,0 +1,9 @@
+public class Encoder {
+    public void encode(short code){
+        System.out.println(code + " => '" + (char)code + "'");
+    }
+
+    public void decode(char symbol){
+        System.out.println("'" + symbol + "' => " + (int)symbol);
+    }
+}
