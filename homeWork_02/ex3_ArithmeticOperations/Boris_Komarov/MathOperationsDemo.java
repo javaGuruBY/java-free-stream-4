eeafadee8dd0f2884b63502fad852e2e892e5334
Boris Komarov <61880815+SimonDubis@users.noreply.github.com>
2020-03-23 14:42:12 +0300

    Adding my 3rd task of the 2nd homework

---
+++ homeWork_02/ex3_ArithmeticOperations/Boris_Komarov/MathOperationsDemo.java
@@ -0,0 +1,14 @@
+package Boris_Komarov;
+
+public class MathOperationsDemo {
+    public static void main(String[] args) {
+        int number1 = 50;
+        int number2 = 6;
+        System.out.println("First number = " + number1);
+        System.out.println("Second number = " + number2);
+        System.out.println("Addition = " + (number1 + number2));
+        System.out.println("Subtraction = " + (number1 - number2));
+        System.out.println("Multiplication = " + (number1 * number2));
+        System.out.println("Division = " + (number1 / number2));
+    }
+}
