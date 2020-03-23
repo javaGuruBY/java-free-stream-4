83fd5c8a4e6464fc7e589785c7f66ffd65a3d85b
Tanya Sorokina <62208319+Tane4kka@users.noreply.github.com>
2020-03-23 04:07:38 +0300

    I did my second homework task3.

---
+++ homeWork_02/ex3_ArithmeticOperations/Tanya_Sorokina/MathOperationsDemo.java
@@ -0,0 +1,20 @@
+public class MathOperationsDemo {
+           public static void main(String[] args) {
+               int a = 70;
+               int b = 8;
+
+               int addition = a + b;
+               int difference = a - b;
+               int multiply = a * b;
+               double division = (double)a / b;
+
+               System.out.print("a + b = ");
+               System.out.println(addition);
+               System.out.print("a - b = ");
+               System.out.println(difference);
+               System.out.print("a * b = ");
+               System.out.println(multiply);
+               System.out.print("a / b = ");
+               System.out.println(division);
+           }
+}
