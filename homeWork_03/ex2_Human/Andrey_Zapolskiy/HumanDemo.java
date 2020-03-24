a2a4f625c3edf0a9bc02e59878c90dd5f86b7394
andrey zapolskiy <58439394+asraysun@users.noreply.github.com>
2020-03-24 19:08:26 +0300

    upload 2 files

---
+++ homeWork_03/ex2_Human/Andrey_Zapolskiy/HumanDemo.java
@@ -0,0 +1,9 @@
+package lv.javaguru.demo;
+
+public class HumanDemo {
+    public static void main(String[] args) {
+        Human Human1 = new Human(23, "Jhon");
+        Human1.greet();
+    }
+
+}
