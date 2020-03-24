e056cf3645b40629c37d8260429b99abdf51799f
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2020-03-24 17:53:58 +0300

    HumanDemo.java Update
    fix

--- homeWork_03/ex2_Human/Artemiy_Stankevich/HumanDemo.java
+++ homeWork_03/ex2_Human/Artemiy_Stankevich/HumanDemo.java
@@ -1,30 +1,3 @@
-import java.io.Serializable;
-class Human implements Serializable {
-    String name;
-    int age;
-    Human () {
-    }
-   
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        if (name != "" ) this.name = name; else System.out.println("Error: variable name cannot be empty");
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        if (age > 0) this.age = age; else System.out.println("Error: age cannot be negative or equal to zero");
-    }
-    void greet() {
-        System.out.println("Hi! My name is " + getName() + " , I'm " +  getAge() + " years old");
-    }
-}
-
 public class HumanDemo {
     public static void main(String[] args) {
         Human man = new Human();
