a0315547e9e91308d14a5c6386d144e41ad507b7
andrey zapolskiy <58439394+asraysun@users.noreply.github.com>
2020-03-24 18:53:48 +0300

    Delete HumanDemo.java

--- homeWork_03/ex2_Human/HumanDemo.java
+++
@@ -1,9 +0,0 @@
-package lv.javaguru.demo;
-
-public class HumanDemo {
-    public static void main(String[] args) {
-        Human Human1 = new Human(23, "Jhon");
-        Human1.greet();
-    }
-
-}
