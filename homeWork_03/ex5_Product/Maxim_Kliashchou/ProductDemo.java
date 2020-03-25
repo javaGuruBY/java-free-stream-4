b04b69b47eff99cec8b957d653f456b97a81e4aa
Maxim Kliashchou <61432134+maximkliashchou@users.noreply.github.com>
2020-03-25 21:45:12 +0300

    Update ProductDemo.java

--- homeWork_03/ex5_Product/Maxim_Kliashchou/ProductDemo.java
+++ homeWork_03/ex5_Product/Maxim_Kliashchou/ProductDemo.java
@@ -1,9 +1,14 @@
+import bean.Product;
+import service.ProductService;
+
 public class ProductDemo {
     public static void main(String[] args) {
         Product productOne = new Product("Beer", 2, 20);
         Product productTwo = new Product("Snacks", 1.5, 5);
 
-        productOne.printInformation();
-        productTwo.printInformation();
+        ProductService productService = new ProductService();
+
+        productService.printInformation(productOne);
+        productService.printInformation(productTwo);
     }
 }
