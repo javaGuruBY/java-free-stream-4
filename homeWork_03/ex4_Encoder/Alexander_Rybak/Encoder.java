22fb82e73226b617735ec6ac5b00a5d51bb6d5e0
Alexander1912 <61635826+Alexander-1313@users.noreply.github.com>
2020-03-25 07:07:53 +0300

    Create Encoder.java

---
+++ homeWork_03/ex4_Encoder/Alexander_Rybak/Encoder.java
@@ -0,0 +1,10 @@
+public class Encoder {
+
+    void encode(short code) {
+        System.out.println(code + " = '" + (char) code + "'");
+    }
+
+    void decode(char symbol) {
+        System.out.println("'" + symbol + "' = " + (int) symbol);
+    }
+}
