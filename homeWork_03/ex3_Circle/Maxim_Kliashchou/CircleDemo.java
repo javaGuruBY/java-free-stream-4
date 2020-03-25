bac8216af794bac7cf03c7aadc8296807af5e875
Maxim Kliashchou <61432134+maximkliashchou@users.noreply.github.com>
2020-03-25 21:34:23 +0300

    update class

--- homeWork_03/ex3_Circle/Maxim_Kliashchou/CircleDemo.java
+++ homeWork_03/ex3_Circle/Maxim_Kliashchou/CircleDemo.java
@@ -1,10 +1,15 @@
+import bean.Circle;
+import service.CircleService;
+
 public class CircleDemo {
     public static void main(String[] args) {
         Circle circle = new Circle(5.6);
 
-        System.out.println("Area of circle = " + circle.calculateArea());
+        CircleService circleService = new CircleService();
+
+        System.out.println("Area of circle = " + circleService.calculateArea(circle));
 
         circle.setRadius(10.1);
-        System.out.println("Area of circle with radius = " + circle.calculateArea());
+        System.out.println("Area of circle with radius = " + circleService.calculateArea(circle));
     }
 }
