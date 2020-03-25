73ebf22cac0fed45143836f257ef20f034a56fed
Maxim Kliashchou <61432134+maximkliashchou@users.noreply.github.com>
2020-03-25 21:15:57 +0300

    update dog demo class

--- homeWork_03/ex1_Dog/Maxim_Kliashchou/DogDemo.java
+++ homeWork_03/ex1_Dog/Maxim_Kliashchou/DogDemo.java
@@ -1,8 +1,13 @@
+import bean.Dog;
+import service.DogService;
+
 public class DogDemo {
     public static void main(String[] args) {
         Dog dogOne = new Dog(5, "White", "John");
         Dog dogTwo = new Dog(7, "Black", "Jessy");
         Dog dogThree = new Dog(3, "Grey", "Ann");
+
+        DogService dogService = new DogService();
 
         System.out.println(dogOne);
         System.out.println(dogTwo);
@@ -19,8 +24,8 @@ public class DogDemo {
 
         System.out.println("Dogs are doing something:");
 
-        dogOne.voice();
-        dogTwo.eat();
-        dogThree.sleep();
+        dogService.eat(dogOne);;
+        dogService.sleep(dogTwo);
+        dogService.voice(dogThree);
     }
 }
