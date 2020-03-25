313e963f86eac36d0be6d2c73577380dd96f2755
Maxim Kliashchou <61432134+maximkliashchou@users.noreply.github.com>
2020-03-25 21:34:41 +0300

    add class

---
+++ homeWork_03/ex3_Circle/Maxim_Kliashchou/CircleService.java
@@ -0,0 +1,9 @@
+package service;
+
+import bean.Circle;
+
+public class CircleService {
+    public double calculateArea(Circle circle){
+        return Math.PI * Math.pow(circle.getRadius(), 2);
+    }
+}
