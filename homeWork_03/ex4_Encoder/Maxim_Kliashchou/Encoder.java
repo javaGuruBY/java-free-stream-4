1c7743a83d0427499cd611f0865fbde48962d032
Maxim Kliashchou <61432134+maximkliashchou@users.noreply.github.com>
2020-03-25 21:37:03 +0300

    Delete Encoder.java

--- homeWork_03/ex4_Encoder/Maxim_Kliashchou/Encoder.java
+++
@@ -1,9 +0,0 @@
-public class Encoder {
-    public void encode(short code){
-        System.out.println(code + " => '" + (char)code + "'");
-    }
-
-    public void decode(char symbol){
-        System.out.println("'" + symbol + "' => " + (int)symbol);
-    }
-}
