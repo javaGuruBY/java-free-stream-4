acb895bec77612416e95649c10d3e11d9ba5a8d2
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2020-03-25 21:49:14 +0300

    Create Circle.java
    first file

---
+++ homeWork_03/ex3_Circle/Artemiy_Stankevich/Circle.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+public class Circle implements Serializable {
+
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius > 0) this.radius = radius; else System.out.println("The radius property cannot be less than zero");
+    }
+    Circle() {
+    }
+    Circle(double rad) {
+        if (rad > 0) this.radius = rad; else System.out.println("The radius property cannot be less than zero");
+    }
+    public  double calculateArea() {
+        return this.radius * this.radius * 3.14 ;
+    }
+}
