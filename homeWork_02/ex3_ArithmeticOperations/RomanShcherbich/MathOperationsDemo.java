365572ec7654ae2f4ecec33e24a8756ecb35dfbb
Roman <ilovebfface@gmail.com>
2020-03-26 17:43:20 +0300

    Roman Hw2 Task5

---
+++ homeWork_02/ex3_ArithmeticOperations/RomanShcherbich/MathOperationsDemo.java
@@ -0,0 +1,38 @@
+public class MathOperationsDemo {
+
+  public static void main(String[] args) {
+    Integer a = 5;
+    Integer b = 3;
+    sum(a, b);
+    difference(a, b);
+    multiply(a, b);
+    quotient(a, b);
+  }
+
+  public static int sum(int a, int b) {
+    int result = a + b;
+    System.out.println("a + b = " + result);
+    return result;
+  }
+
+  public static int difference(int a, int b) {
+    int result = a - b;
+    System.out.println("a - b = " + result);
+    return result;
+  }
+
+  public static int multiply(int a, int b) {
+    int result = a * b;
+    System.out.println("a x b = " + result);
+    return result;
+  }
+
+  public static double quotient(int a, int b) {
+    double a1 = (double) a;
+    double b1 = (double) b;
+    double result = a1 / b1;
+    System.out.println("a : b = " + result);
+    return result;
+  }
+
+}
