1c99e7d8a719d8e3eabde609400c20b349e03da1
VictoriaKacer <62204864+VictoriaKacer@users.noreply.github.com>
2020-03-26 17:32:13 +0300

    MathOperationsDemo.java
    my Hw2Task3

---
+++ homeWork_02/ex3_ArithmeticOperations/Victoria_Kacer/MathOperationsDemo.java
@@ -0,0 +1,94 @@
+public class MathOperationsDemo {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to the `Math Operations`!");
+
+            int a = 100;
+            int b = 45;
+
+            System.out.println("1) Addition `+`:"); //1
+            System.out.print("a = ");           //2
+            System.out.print(a);                //3
+            System.out.print(", ");              //4
+            System.out.print("b = ");           //5
+            System.out.print(b);                //6
+
+            int sum = a + b;
+
+            System.out.println();//7
+            System.out.print("a + b = ");        //8
+            System.out.print(a);                 //9
+            System.out.print(" + ");             //10
+            System.out.print(b);                 //11
+            System.out.print(" = ");             //12
+            System.out.print(sum);               //13
+
+            byte c = 9;
+            byte d = 6;
+
+            System.out.println();
+
+            System.out.println("2) Subtraction `-`:"); //1
+            System.out.print("c = ");           //2
+            System.out.print(c);                //3
+            System.out.print(", ");              //4
+            System.out.print("d = ");           //5
+            System.out.print(d);                //6
+
+            int subtr = c - d;
+
+            System.out.println();//7
+            System.out.print("c - d = ");        //8
+            System.out.print(c);                 //9
+            System.out.print(" - ") ;             //10
+            System.out.print(d);                 //11
+            System.out.print(" = ");             //12
+            System.out.print(subtr);               //13
+
+            long e = 20;
+            long f = 4;
+
+            System.out.println();
+
+            System.out.println("3) Division `/`:"); //1
+            System.out.print("e = ");           //2
+            System.out.print(e);                //3
+            System.out.print(", ");              //4
+            System.out.print("f = ");           //5
+            System.out.print(f);                //6
+
+            long divis = e / f;
+
+            System.out.println();//7
+            System.out.print("e / f = ");        //8
+            System.out.print(e);                 //9
+            System.out.print(" / ") ;             //10
+            System.out.print(f);                 //11
+            System.out.print(" = ");             //12
+            System.out.print(divis);               //13
+
+            short k = 7;
+            short m = 3;
+
+            System.out.println();
+
+            System.out.println("4) Multiplication `*`:"); //1
+            System.out.print("k = ");           //2
+            System.out.print(k);                //3
+            System.out.print(", ");              //4
+            System.out.print("m = ");           //5
+            System.out.print(m);                //6
+
+            int mult = (k * m);
+
+            System.out.println();//7
+            System.out.print("k * m = ");        //8
+            System.out.print(k);                 //9
+            System.out.print(" * ") ;             //10
+            System.out.print(m);                 //11
+            System.out.print(" = ");             //12
+            System.out.print(mult);               //13
+
+
+    }
+}
