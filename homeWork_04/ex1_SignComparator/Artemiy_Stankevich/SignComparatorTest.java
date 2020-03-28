8ffae4a2559094280179d576e412ca97563af84e
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2020-03-28 19:45:49 +0300

    Create SignComparatorTest.java
    second file

---
+++ homeWork_04/ex1_SignComparator/Artemiy_Stankevich/SignComparatorTest.java
@@ -0,0 +1,8 @@
+public class SignComparatorTest {
+    public static void main(String[] args) {
+        SingComparator sC = new SingComparator();
+        System.out.println(sC.compare(20));
+        System.out.println(sC.compare(-200));
+        System.out.println(sC.compare(0));
+    }
+}
