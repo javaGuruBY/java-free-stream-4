06549d2301e7951afd2094aa2ef6bc35d2f4d9ed
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2020-03-28 21:37:07 +0300

    Create SignComparatorTest.java
    third file

---
+++ homeWork_04/ex1_SignComparator/Artemiy_Stankevich/SignComparatorTest.java
@@ -0,0 +1,27 @@
+public class SignComparatorTest {
+    private SingComparatorService scs = new SingComparatorService();
+    public void compareTestPositiveNumber () {
+        System.out.println("Initializing test 1 - checking a number for a positive value");
+        int numPositive = 5;
+        String expectedBehaviourForPositiveNumber5 = "number is positive";
+        String actualResult = scs.compare(numPositive);
+        System.out.print("Test 1 = ");
+        System.out.println(expectedBehaviourForPositiveNumber5 == actualResult);
+    }
+    public void compareTestNegativeNumber () {
+        System.out.println("Initializing test 2 - checking a number for a negative value");
+        int numNegative = -200;
+        String expectedBehaviourForPositiveNumber5 = "number is negative";
+        String actualResult = scs.compare(numNegative);
+        System.out.print("Test 2 = ");
+        System.out.println(expectedBehaviourForPositiveNumber5 == actualResult);
+    }
+    public void compareTestEqualZeroNumber () {
+        System.out.println("Initializing test 3 - checking a number for a zero value");
+        int numZero = 0;
+        String expectedBehaviourForPositiveNumber5 = "Number is equal to zero";
+        String actualResult = scs.compare(numZero);
+        System.out.print("Test 3 = ");
+        System.out.println(expectedBehaviourForPositiveNumber5 == actualResult);
+    }
+}
