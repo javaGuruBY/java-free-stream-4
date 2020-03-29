b16014f14a0579a0eae2b988cfdb15a588aab60a
Alexander1912 <61635826+Alexander-1313@users.noreply.github.com>
2020-03-29 12:41:13 +0300

    Create User.java

---
+++ homeWork_04/ex5_UserLoginService/Alexander_Rybak/User.java
@@ -0,0 +1,55 @@
+public class User {
+    private String password;
+    private String login;
+    private boolean isBlocked = false;
+    private int countOfAttemptsToEnterTheAccount = 0;
+
+    public User(String login, String password) {
+        this.password = password;
+        this.login = login;
+    }
+
+    public void resetLoginAttempts() {
+        setCountOfAttemptsToEnterTheAccount(0);
+    }
+
+    public void blockUser() {
+        setBlocked(true);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        isBlocked = blocked;
+    }
+
+    public int getCountOfAttemptsToEnterTheAccount() {
+        return countOfAttemptsToEnterTheAccount;
+    }
+
+    public void setCountOfAttemptsToEnterTheAccount(int countOfAttemptsToEnterTheAccount) {
+        this.countOfAttemptsToEnterTheAccount = countOfAttemptsToEnterTheAccount;
+    }
+
+    public int increaseCountOfAttemptsToEnterTheAccount() {
+        return ++(this.countOfAttemptsToEnterTheAccount);
+    }
+}
