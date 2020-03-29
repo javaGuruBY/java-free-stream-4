b170b2a43134002d805ce3b7bb9bef0d8b05b3b3
Alexander1912 <61635826+Alexander-1313@users.noreply.github.com>
2020-03-29 12:35:10 +0300

    Create PhraseAnalyser.java

---
+++ homeWork_04/ex2_Phrase_Analyser/Alexander_Rybak/PhraseAnalyser.java
@@ -0,0 +1,10 @@
+public class PhraseAnalyser {
+    public String analyse(String text) {
+        int firstIndex = text.indexOf("Make");
+        int lastIndex = text.lastIndexOf("great again");
+
+        if (firstIndex == 0 && lastIndex == text.length() - 11) return "It stands no chance";
+        else if (firstIndex == 0 || lastIndex == text.length() - 11) return "It could be worse";
+        else return "It is fine, really";
+    }
+}
