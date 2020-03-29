bdb8cc3b729dcfbd805e852515c2d50ea0262f66
Alexander1912 <61635826+Alexander-1313@users.noreply.github.com>
2020-03-29 12:32:54 +0300

    Create SignCoparator.java

---
+++ homeWork_04/ex1_SignComparator/Alexander_Rybak/SignCoparator.java
@@ -0,0 +1,7 @@
+public class SignComparator {
+    public String compare(int number) {
+        if (number < 0) return "Number is negative";
+        else if (number > 0) return "Number is positive";
+        else return "Number is equal to zero";
+    }
+}
