d1f986c3863f0df7b95fd0d42fe1a6c4bad38a17
JakeE1 <45400738+JakeE1@users.noreply.github.com>
2020-03-30 17:44:08 +0300

    add Dogdemo

---
+++ homeWork_03/ex1_Dog/Vlad_Ch/DogDemo.java
@@ -0,0 +1,12 @@
+public class DogDemo {
+
+  public static void main(String[] args) {
+  
+    Dog Yumi = new Dog("Yumi", 1 ,"Golden black");
+
+    Yumi.voice();
+    Yumi.eat();
+    Yumi.sleep();
+
+  }
+}
