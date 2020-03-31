ce8f1b242526601e4f057d9f210f223a2d270e22
Максим Клещёв <mmkleschev@gmail.com>
2020-03-31 21:36:58 +0300

    create spring test

---
+++ homeWork_04/ex5_UserLoginService/Maxim_Kliashchou/TestSpring/src/test/java/com/mmkleschev/TestSpring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmkleschev.TestSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
