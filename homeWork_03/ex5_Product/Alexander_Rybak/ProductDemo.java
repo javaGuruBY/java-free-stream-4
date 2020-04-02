1309215778369c03baf02ebb90f13cf37e34a5eb
Alexander1912 <61635826+Alexander-1313@users.noreply.github.com>
2020-04-02 19:38:54 +0300

    Update ProductDemo.java

--- homeWork_03/ex5_Product/Alexander_Rybak/ProductDemo.java
+++ homeWork_03/ex5_Product/Alexander_Rybak/ProductDemo.java
@@ -1,14 +1,24 @@
+import bean.Product;
+import service.ProductService;
+
 public class ProductDemo {
     public static void main(String[] args) {
         Product milk = new Product("Milk", 2, 20);
+        Product cheese = new Product("Cheese", 10, 17);
 
-        milk.printInformation();
+        ProductService productService = new ProductService();
+
+        productService.printInformation(milk);
+        productService.printInformation(cheese);
 
         milk.setDiscount(50);
         milk.setRegularPrice(4);
-
+        cheese.setRegularPrice(18);
+        cheese.setDiscount(14)
+        ;
         System.out.println("Information about milk after changes:");
 
-        milk.printInformation();
+        productService.printInformation(milk);
+        productService.printInformation(cheese);
     }
 }
