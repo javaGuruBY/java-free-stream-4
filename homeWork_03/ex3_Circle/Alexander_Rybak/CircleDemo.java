981e5c9b6ecaf0d8b7b370ffffb53f455467f922
Alexander1912 <61635826+Alexander-1313@users.noreply.github.com>
2020-04-02 19:35:19 +0300

    Update CircleDemo.java

--- homeWork_03/ex3_Circle/Alexander_Rybak/CircleDemo.java
+++ homeWork_03/ex3_Circle/Alexander_Rybak/CircleDemo.java
@@ -1,17 +1,23 @@
+import bean.Circle;
+import service.CircleService;
+
+
 public class CircleDemo {
     public static void main(String[] args) {
         Circle firstCircle = new Circle(5);
         Circle secondCircle = new Circle(3);
 
-        System.out.println("Area of the first circle with radius = " + firstCircle.getRadius() + " is " + firstCircle.calculateArea());
-        System.out.println("Area of the second circle with radius = " + secondCircle.getRadius() + " is " + secondCircle.calculateArea());
+        CircleService circleService = new CircleService();
+
+        System.out.println("Area of the first circle with radius = " + firstCircle.getRadius() + " is " + circleService.calculateArea(firstCircle));
+        System.out.println("Area of the second circle with radius = " + secondCircle.getRadius() + " is " + circleService.calculateArea(secondCircle));
 
         firstCircle.setRadius(8);
         secondCircle.setRadius(15);
 
         System.out.println("Area of these two circles:");
 
-        System.out.println("Area of the first circle with radius = " + firstCircle.getRadius() + " is " + firstCircle.calculateArea());
-        System.out.println("Area of the second circle with radius = " + secondCircle.getRadius() + " is " + secondCircle.calculateArea());
+        System.out.println("Area of the first circle with radius = " + firstCircle.getRadius() + " is " + circleService.calculateArea(firstCircle));
+        System.out.println("Area of the second circle with radius = " + secondCircle.getRadius() + " is " + circleService.calculateArea(secondCircle));
     }
 }
