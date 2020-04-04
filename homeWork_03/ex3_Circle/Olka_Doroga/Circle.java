16104e756f98c119b866c8ab44d5e52d5864ff0f
olka_doroga <62174076+olkadoroga@users.noreply.github.com>
2020-04-04 19:27:40 +0300

    Olka_Doroga Hw3 Task3

--- homeWork_03/ex3_Circle/Olka_Doroga/Circle.java
+++ homeWork_03/ex3_Circle/Olka_Doroga/Circle.java
@@ -1,1 +1,12 @@
+public class Circle {
+    public double radius;
+    public double squareCircle;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
 
+    void calculateArea() {
+        squareCircle = 3.14 * (radius * radius);
+        System.out.println(squareCircle);
+    }
+}
---
+++ homeWork_03/ex3_Circle/Olka_Doroga/CircleDemo.java
@@ -0,0 +1,7 @@
+public class CircleDemo {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3);
+
+        circle.calculateArea();
+    }
+}
