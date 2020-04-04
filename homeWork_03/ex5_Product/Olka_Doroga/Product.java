d94310041bafb57e83b6292e4a2ef79cc46aa177
olka_doroga <62174076+olkadoroga@users.noreply.github.com>
2020-04-04 20:25:23 +0300

    Olka_Doroga Hw3 Task5

--- homeWork_03/ex5_Product/Olka_Doroga/Product.java
+++ homeWork_03/ex5_Product/Olka_Doroga/Product.java
@@ -1,1 +1,23 @@
+public class Product {
 
+    String name;
+    double regularPrice;
+    double discount;
+    double actualPrice;
+
+    public Product(String name, double regularPrice, double discount) {
+        this.name = name;
+        this.regularPrice = regularPrice;
+        this.discount = discount;
+    }
+
+    public void actualPrice() {
+        actualPrice = regularPrice * 0.80;
+   }
+
+   public void printInfo(){
+        System.out.println("Name: " + name + ", Regular Price = "
+                + regularPrice + ", Discount = " + discount
+                + "%, Actual price = " + actualPrice + " ");
+        }
+}
