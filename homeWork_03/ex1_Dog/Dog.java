8e0d333902ea2485cac8f63a0f37bfbdf0aaeb28
olka_doroga <62174076+olkadoroga@users.noreply.github.com>
2020-04-04 17:30:00 +0300

    Update Dog.java

--- homeWork_03/ex1_Dog/Dog.java
+++ homeWork_03/ex1_Dog/Dog.java
@@ -3,20 +3,15 @@ public class Dog {
     public String color;
     public String name;
 
-
     public Dog(int age, String color, String name){
-
         this.age = age;
         this.color = color;
         this.name = name;
     }
 
-
-
     void printInfo(){
         System.out.println(name + " " + age + " " + color);
     }
-
     void voice() {
         System.out.println("Aff");
     }
@@ -26,43 +21,4 @@ public class Dog {
     void sleep() {
         System.out.println("Hhrrr");
     }
-        }
-
-    public void voice() {
-        System.out.println("Woff-Woff");
-    }
-
-    public void eat() {
-        System.out.println("ham-ham");
-    }
-
-    public void sleep() {
-        System.out.println("Hrr-r-r");
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
-
-
