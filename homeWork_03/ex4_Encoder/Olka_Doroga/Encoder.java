5933259fd94b4cd7365a3ed1718c83c4fc737b71
olka_doroga <62174076+olkadoroga@users.noreply.github.com>
2020-04-04 18:03:28 +0300

    Completed Hw3 T4

--- homeWork_03/ex4_Encoder/Olka_Doroga/Encoder.java
+++ homeWork_03/ex4_Encoder/Olka_Doroga/Encoder.java
@@ -1,1 +1,12 @@
+public class Encoder {
 
+    void encode(short code) {
+
+        System.out.println(code + " = " + (char) code + "");
+    }
+
+    void decode(char symbol) {
+         System.out.println("" + symbol + " = " + (int) symbol);
+
+    }
+}
