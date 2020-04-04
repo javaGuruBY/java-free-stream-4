2831c45c78b78cec2e19c60bea77d36051297af5
olka_doroga <62174076+olkadoroga@users.noreply.github.com>
2020-04-04 19:06:10 +0300

    Olka_DorogaHw2Task4

--- homeWork_02/ex4_RandomNumberGenerator/Olka_Doroga/RandomGeneratorDemo.java
+++ homeWork_02/ex4_RandomNumberGenerator/Olka_Doroga/RandomGeneratorDemo.java
@@ -1,1 +1,18 @@
+import java.util.Random;
 
+public class RandomGeneratorDemo {
+    public static void main(String [] args) {
+
+        Random randomGenerator = new Random();
+        int boundRandom1 = randomGenerator.nextInt(250);
+        System.out.println("1: " + boundRandom1);
+        int boundRandom2 = randomGenerator.nextInt(250);
+        System.out.println("2: " + boundRandom2);
+        int boundMath = (int) (Math.random()* 251);
+        System.out.println("3: " + boundMath);
+
+        int sum = boundRandom1 + boundRandom2 + boundMath;
+        System.out.println("Sum = " + sum);
+    }
+
+}
