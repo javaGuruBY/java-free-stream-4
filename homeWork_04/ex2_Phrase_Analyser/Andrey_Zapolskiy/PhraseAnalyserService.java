a558790186c084aa4e9e4031fda4b0a88a91c56f
andrey zapolskiy <58439394+asraysun@users.noreply.github.com>
2020-04-06 08:51:30 +0300

    Add files via upload

---
+++ homeWork_04/ex2_Phrase_Analyser/Andrey_Zapolskiy/PhraseAnalyserService.java
@@ -0,0 +1,17 @@
+
+
+public class PhraseAnalyserService {
+    String phraseOne = "It stands no chance";
+    String phraseTwo = "It could be worse";
+    String phraseNoOne = "It is fine, really";
+
+    public String analyse(String text) {
+        String [] arraysString = text.split(" ");
+
+        if (arraysString[0].equals("Make") && arraysString[arraysString.length-2].equals("great") &&
+                arraysString[arraysString.length-1].equals("again")) return phraseOne;
+        else if (arraysString[0].equals("Make") || arraysString[arraysString.length-2].equals("great") &&
+                arraysString[arraysString.length-1].equals("again")) return phraseTwo;
+        else return phraseNoOne;
+    }
+}
