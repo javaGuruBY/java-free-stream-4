aee6fda76b60ed3eebab1b1570b5607fd30466b6
abbas29n <abbas.n@outlook.com>
2020-04-12 17:12:48 +0300

    spring framework demo

---
+++ Spring_Example/stream4demo/src/test/java/by/jrr/stream4demo/Stream4demoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.jrr.stream4demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stream4demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
